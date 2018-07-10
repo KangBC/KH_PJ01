d68e4155c11243cc2f110f4a6ae656ca9fc32f54
BIT\lenovo <Kang.b.c.1208@gmail.com>
2018-07-10 10:35:18 +0900

    한글-test

--- Test/src/main.java
+++ Test/src/main.java
@@ -4,9 +4,9 @@ public class main {
 	public static void main(String[] args) {
 
 		System.out.println("bit : check");
-		System.out.println("����  : check ");
-		System.out.println("���� 2�� Ȯ�� : check");
-		System.out.println("현우1차테스트");
+		System.out.println("현석  : check ");
+		System.out.println("현석 2차 테스트 : check");
+		System.out.println("현우 1차 테스트");
 	}
 
 }
