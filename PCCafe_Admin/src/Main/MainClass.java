08b5fbadab9cd95c9c4373994cfc471b2ae7188f
DESKTOP-HAS39O4\hyunwoo <dldjsl2@naver.com>
2018-07-11 13:44:52 +0900

    test02

--- PCCafe_Admin/src/Main/MainClass.java
+++ PCCafe_Admin/src/Main/MainClass.java
@@ -1,9 +1,37 @@
 package Main;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import view.Administrator;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("예쓰~~~~");
-		System.out.println("예쓰~111111111111111111111~~~");
+
+		try {
+			UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		JFrame.setDefaultLookAndFeelDecorated(true);
+
+		try {
+			Administrator frame = new Administrator();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
