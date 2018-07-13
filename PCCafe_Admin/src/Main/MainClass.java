8f59fe72daed78b7e5c71631daabe601ebf383bc
BIT\lenovo <Kang.b.c.1208@gmail.com>
2018-07-13 22:29:46 +0900

    no message

--- PCCafe_Admin/src/Main/MainClass.java
+++ PCCafe_Admin/src/Main/MainClass.java
@@ -4,39 +4,27 @@ import java.sql.SQLException;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import DB.DBConnection;
 import Singleton.Singleton;
 import View.BbsUpateview;
+import View.ControlView;
 
 public class MainClass {
 
-<<<<<<< HEAD
-	public static void main(String[] args) throws Exception{
-=======
-	public static void main(String[] args) throws SQLException {
 
+	public static void main(String[] args) throws Exception{
 		// DB
 		new DBConnection().makeConnection();
 
 		// Single
 		Singleton sc = Singleton.getInstance();
 
->>>>>>> KH_MiniProject/hyunwoo
-
 		// Look & Feel
-		try {
-			UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
-<<<<<<< HEAD
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (InstantiationException e1) {
-			e1.printStackTrace();
-		} catch (IllegalAccessException e1) {
-			e1.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e1) {
-			e1.printStackTrace();
-		}
+		
+		UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
+
 		JFrame.setDefaultLookAndFeelDecorated(true);
 
 		ControlView frame = new ControlView();
